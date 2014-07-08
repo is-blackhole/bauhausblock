5382bceabc72a0de90b3f878a9fc4789ca405879
Jelle Spekken <jspekken@gmail.com>
2014-07-09 00:10:01 +0200

    updated builder

--- src/KraftHaus/BauhausBlock/Builder.php
+++ src/KraftHaus/BauhausBlock/Builder.php
@@ -3,15 +3,47 @@
 namespace KraftHaus\BauhausBlock;
 
 use Illuminate\Support\Str;
+use KraftHaus\BauhausBlock\Resolver\OptionResolver;
 
 class Builder
 {
 
+	protected $optionResolver;
+
 	public function render($type)
 	{
+		$this->setOptionResolver(new OptionResolver);
+
 		$class = \Str::studly($type . 'Block');
-		return (new $class)
-			->execute();
+		$block = new $class;
+		$block->setDefaults($this->getOptionResolver());
+
+		return $block->execute();
+	}
+
+	/**
+	 * Set the OptionResolver object.
+	 *
+	 * @param  OptionResolver $optionResolver
+	 *
+	 * @access public
+	 * @return Builder
+	 */
+	public function setOptionResolver($optionResolver)
+	{
+		$this->optionResolver = $optionResolver;
+		return $this;
+	}
+
+	/**
+	 * Get the OptionResolver object.
+	 * 
+	 * @access public
+	 * @return OptionResolver
+	 */
+	public function getOptionResolver()
+	{
+		return $this->optionResolver;
 	}
 
 }
