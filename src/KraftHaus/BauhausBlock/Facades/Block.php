3f67ff76b34ee1ea2240f4864264277c78869a87
Jelle Spekken <jelle.spekken@centagon.com>
2014-07-09 17:13:10 +0200

    facade added

--- src/KraftHaus/BauhausBlock/Facades/Block.php
+++ src/KraftHaus/BauhausBlock/Facades/Block.php
@@ -2,8 +2,21 @@
 
 namespace KraftHaus\BauhausBlock\Facades;
 
+/**
+ * This file is part of the KraftHaus BauhausBlock package.
+ *
+ * (c) KraftHaus <hello@krafthaus.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Block
+ * @package KraftHaus\BauhausBlock\Facades
+ */
 class Block extends Facade
 {
 
