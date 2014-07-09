1a76a06fe91322e9b8101f394b12bda459a2aeec
Jelle Spekken <jelle.spekken@centagon.com>
2014-07-09 17:13:20 +0200

    updated functionlality

--- src/KraftHaus/BauhausBlock/Block.php
+++ src/KraftHaus/BauhausBlock/Block.php
@@ -2,9 +2,112 @@
 
 namespace KraftHaus\BauhausBlock;
 
+/**
+ * This file is part of the KraftHaus Bauhaus package.
+ *
+ * (c) KraftHaus <hello@krafthaus.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use KraftHaus\BauhausBlock\Resolver\OptionResolver;
+
+/**
+ * Class Block
+ * @package KraftHaus\BauhausBlock
+ * @abstract
+ */
 abstract class Block
 {
 
+	/**
+	 * Holds the OptionResolver object.
+	 * @var OptionResolver
+	 */
+	protected $optionResolver;
+
+	/**
+	 * Holds the cache timeout.
+	 * @var int
+	 */
+	protected $ttl = false;
+
+	/**
+	 * Configure the current block instance.
+	 *
+	 * @param  OptionResolver $resolver
+	 *
+	 * @access public
+	 * @return mixed
+	 * @abstract
+	 */
+	abstract public function configure(OptionResolver $resolver);
+
+	/**
+	 * Execute/setup the current block instance.
+	 *
+	 * @access public
+	 * @return mixed
+	 * @abstract
+	 */
 	abstract public function execute();
+
+	/**
+	 * Set the OptionResolver object.
+	 *
+	 * @param  OptionResolver $optionResolver
+	 *
+	 * @access public
+	 * @return Builder
+	 */
+	public function setOptionResolver($optionResolver)
+	{
+		$this->optionResolver = $optionResolver;
+		return $this;
+	}
+
+	/**
+	 * Get the OptionResolver object.
+	 *
+	 * @access public
+	 * @return OptionResolver
+	 */
+	public function getOptionResolver()
+	{
+		return $this->optionResolver;
+	}
+
+	/**
+	 * Render the block.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function render()
+	{
+		// intentionally left blank
+	}
+
+	/**
+	 * @param  int $ttl
+	 * @return $this
+	 */
+	public function setTtl($ttl)
+	{
+		$this->ttl = $ttl;
+		return $this;
+	}
+
+	/**
+	 * Get the block ttl.
+	 *
+	 * @access public
+	 * @return int
+	 */
+	public function getTtl()
+	{
+		return $this->ttl;
+	}
 
 }
--- src/KraftHaus/BauhausBlock/Builder.php
+++ src/KraftHaus/BauhausBlock/Builder.php
@@ -2,48 +2,35 @@
 
 namespace KraftHaus\BauhausBlock;
 
+/**
+ * This file is part of the KraftHaus Bauhaus package.
+ *
+ * (c) KraftHaus <hello@krafthaus.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Illuminate\Support\Str;
 use KraftHaus\BauhausBlock\Resolver\OptionResolver;
 
+/**
+ * Class Builder
+ * @package KraftHaus\BauhausBlock
+ */
 class Builder
 {
 
-	protected $optionResolver;
-
 	public function render($type)
 	{
-		$this->setOptionResolver(new OptionResolver);
+		$class = Str::studly($type . 'Block');
+		$block = (new $class)
+			->setOptionResolver(new OptionResolver);
 
-		$class = \Str::studly($type . 'Block');
-		$block = new $class;
-		$block->setDefaults($this->getOptionResolver());
+		$block->configure($block->getOptionResolver());
+		$block->execute();
 
-		return $block->execute();
-	}
-
-	/**
-	 * Set the OptionResolver object.
-	 *
-	 * @param  OptionResolver $optionResolver
-	 *
-	 * @access public
-	 * @return Builder
-	 */
-	public function setOptionResolver($optionResolver)
-	{
-		$this->optionResolver = $optionResolver;
-		return $this;
-	}
-
-	/**
-	 * Get the OptionResolver object.
-	 * 
-	 * @access public
-	 * @return OptionResolver
-	 */
-	public function getOptionResolver()
-	{
-		return $this->optionResolver;
+		return $block->render();
 	}
 
 }
